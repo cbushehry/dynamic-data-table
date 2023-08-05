faf4427136a5efc7fa18cff6a65e733ed4b4cf8f
c.bushehry <c.bushehry@gmail.com>
2023-08-05 15:12:26 -0700

    added ability to sort by EUR, fixed cars.json file

--- cardcollector.js
+++ cardcollector.js
@@ -1,36 +1,46 @@
 async function loadCards(){
     try{
-        const response = await fetch('http://127.0.0.1:5500/CODE/ZENVAprojects/dynamic-data-table/cards.json');     //local path
+        const response = await fetch('http://127.0.0.1:5500/CODE/ZENVAprojects/dynamic-data-table/cards.json');
         let cardsJson = await response.json();
         let cardTable = document.getElementById("myCards");
         let onlyRares = document.getElementById("raresOnly");
+        let inEuro = document.getElementById("currency");
+        const EURO_CONVERSION = 0.87;
+        let priceMultiplier;
 
         var cards = cardsJson.map(function(card){
             var cardsToShow = {
                 "id": card.id,
                 "name": card.name,
-                "price": card.price,
+                "priceUSD": card.priceUSD,
                 "isRare": card.isRare
             }
 
             return cardsToShow;
         })
-        
-        cardTable.innerHTML = "<tr><td>ID</td><td>Name</td><td>Price</td><td>Is Rare</td>";
-        let rareCards = cards.filter(obj => obj.isRare == true).map(obj => ({"id":obj.id, "name":obj.name, "price":obj.price,"isRare":obj.isRare}));
 
+        if(inEuro.checked){
+            priceMultiplier = EURO_CONVERSION;
+        }else{
+            priceMultiplier = 1;
+        }
+
+        cardTable.innerHTML = "<tr><td>ID</td><td>Name</td><td>Price</td><td>Is Rare</td>";
+        let rareCards = cards.filter(obj => obj.isRare == true).map(obj => ({"id":obj.id, "name":obj.name, "priceUSD":obj.priceUSD,"isRare":obj.isRare}));
+        
         if(onlyRares.checked){
             for (var i = 0; i < rareCards.length; i++){
-                cardTable.innerHTML += "" + rareCards[i].id + "" + rareCards[i].name + "" + rareCards[i].price + "" + rareCards[i].isRare + "";
+                cardTable.innerHTML += "<tr><td>" + rareCards[i].id + "</td><td>" + rareCards[i].name + "</td><td>" + Math.round(rareCards[i].priceUSD * priceMultiplier) + "</td><td>" + rareCards[i].isRare + "</td></tr>";
             }
         }else{
             for (var i = 0; i < cards.length; i++){
-                cardTable.innerHTML += "" + cards[i].id + "" + cards[i].name + "" + cards[i].price + "" + cards[i].isRare + "";
+                cardTable.innerHTML += "<tr><td>" + cards[i].id + "</td><td>" + cards[i].name + "</td><td>" + Math.round(cards[i].priceUSD * priceMultiplier) + "</td><td>" + cards[i].isRare + "</td></tr>";
             }
         }
     }
     catch(err){
         alert(err);
     }
-
+    
 }
+
